43f798ae7cb38cc53033de7c0e56be0352f3016c
Jaakko252525 <124563002+Jaakko252525@users.noreply.github.com>
2023-08-03 21:01:24 +0300

    backend upgrades

--- backend/src/routes/diagnosesRoute.ts
+++ backend/src/routes/diagnosesRoute.ts
@@ -31,6 +31,61 @@ router.get('', (_req, res) => {
   
   });
   
+//post req
+router.post('', (req, res) => {
+
+  console.log('inside post!')
+
+
+
+  try {
+
+    // this commented code should check if correct datatype but it dont work
+    /*
+    // checks if solid input
+    const newPatientEntry = toNewPatientEntry(req.body);
+  
+    // logging newPatientEntry
+    console.log('logging newPatientEntry', newPatientEntry);
+  
+    console.log('object values', newPatientEntry.name, 'typeof', typeof newPatientEntry.name);
+      
+    */
+   
+  
+    // interface
+    interface RequestBody {
+      name: string,
+      code: string,
+      latin: string
+    }
+  
+    // body     
+    const { name, code, latin } = req.body as RequestBody;
+
+  
+    // using patientService addPatient  
+    const addedEntry = diagnoseServices.addDiagnose(
+       name,
+       code,
+       latin
+      );
+  
+      console.log('Adding this:', addedEntry);
+      
+      
+    res.json(addedEntry);
+  
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong in POST.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+  });
+
+
 
 
 export default router;
--- backend/src/services/diagnoseService.ts
+++ backend/src/services/diagnoseService.ts
@@ -14,18 +14,39 @@ import diagnoseData from '../data/diagnoses';
 // importing diagnoseEntry
 import { DiagnoseEntry } from '../types/diagnoseTypes';
 
+import diagnoseEntries from '../data/diagnoses';
 
 const diagnoses: DiagnoseEntry[] = diagnoseData;
 
-
+import patients from '../data/patients'
 
 const getEntries = () => {
     return diagnoses;
 };
 
 
-const addDiagnose = () => {
-    return null;
+
+
+// adding diagnose
+export const addDiagnose = (name: string, code: string, latin: string): DiagnoseEntry => {
+
+
+
+
+
+    const newDiagnoseEntry = {
+        code,
+        name,
+        latin
+    }
+
+    diagnoseEntries.push(newDiagnoseEntry);
+    
+    const patient = patients.find(p => p.name === name)
+
+    console.log('this is the patient', patient)
+
+    return newDiagnoseEntry;
 };
 
 
